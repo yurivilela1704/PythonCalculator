5c3389dfb67de59f613fc7b76c3a37ca35fb8914
Yuri Vilela <yurimarquez1704@gmail.com>
2021-04-21 16:46:28 -0300

    Adicionando nova versão com raiz quadrada

---
+++ Calculadora/Calculora_v2.py
@@ -0,0 +1,62 @@
+print(15 * '#', 'PYTHON CALCULATOR', 15 * '#')
+print('Select the number for the operation you want?\n')
+print('1 - SUM')
+print('2 - SUBTRACTION')
+print('3 - MULTIPLICATION')
+print('4 - DIVISION')
+print('5 - DIVISION(with module)')
+print('6 - ENTIRE PART OF THE DIVISION\n')
+
+
+def sum_option(x, y):
+    return x + y
+
+
+def sub_option(x, y):
+    return x - y
+
+
+def mult_option(x, y):
+    return x * y
+
+
+def div_option(x, y):
+    return x / y
+
+
+def module_option(x, y):
+    return x % y
+
+
+def entire_part(x, y):
+    return x // y
+
+
+option = int(input('Select one option (1, 2, 3, 4, 5, 6): '))
+first_number = float(input('Type it the first number: '))
+second_number = float(input('Type it the second number: '))
+
+
+if option == 1:
+    print('OPERATION OF SUM')
+    print(f'{first_number} + {second_number} = {sum_option(first_number, second_number)}')
+elif option == 2:
+    print('OPERATION OF SUBTRACTION')
+    print(f'{first_number} - {second_number} = {sub_option(first_number, second_number)}')
+elif option == 3:
+    print('OPERATION OF MULTIPLICATION')
+    print(f'{first_number} * {second_number} = {mult_option(first_number, second_number)}')
+elif option == 4:
+    print('OPERATION OF DIVISION')
+    print(f'{first_number} / {second_number} = {div_option(first_number, second_number)}')
+elif option == 5:
+    print('OPERATION OF DIVISION WITH MODULE')
+    print(f'{first_number} / {second_number} = {div_option(first_number, second_number)}')
+    print(f'{first_number} % {second_number} = {module_option(first_number, second_number)}')
+elif option == 6:
+    print('OPERATION OF ENTIRE PART OF THE DIVISION')
+    print(f'{first_number} // {second_number} = {entire_part(first_number, second_number)}')
+else:
+    print(20 * '*')
+    print('ERRO, REBOOT THE CODE')
+    print(20 * '*')
--- Calculadora/Calculora_v3.py
+++ Calculadora/Calculora_v3.py
@@ -1,3 +1,5 @@
+import math
+
 print(15 * '#', 'PYTHON CALCULATOR', 15 * '#')
 print('Select the number for the operation you want?\n')
 print('1 - SUM')
@@ -5,7 +7,8 @@ print('2 - SUBTRACTION')
 print('3 - MULTIPLICATION')
 print('4 - DIVISION')
 print('5 - DIVISION(with module)')
-print('5 - ENTIRE PART OF THE DIVISION\n')
+print('6 - ENTIRE PART OF THE DIVISION')
+print('7 - SQUARE ROOT\n')
 
 
 def sum_option(x, y):
@@ -32,30 +35,42 @@ def entire_part(x, y):
     return x // y
 
 
-option = int(input('Select one option (1, 2, 3, 4): '))
-first_number = float(input('Type it the first number: '))
-second_number = float(input('Type it the second number: '))
+def square_root(x):
+    return math.sqrt(x)
+
+
+option = int(input('Select one option (1, 2, 3, 4, 5, 6, 7): '))
+
+
+if 0 < option < 6:
+    first_number = float(input('Type it the first number: '))
+    second_number = float(input('Type it the second number: '))
+else:
+    one_number = float(input('Type it one number: '))
 
 
 if option == 1:
-    print('OPERATION OF SUM')
+    print('OPERATION OF SUM:')
     print(f'{first_number} + {second_number} = {sum_option(first_number, second_number)}')
 elif option == 2:
-    print('OPERATION OF SUBTRACTION')
+    print('OPERATION OF SUBTRACTION:')
     print(f'{first_number} - {second_number} = {sub_option(first_number, second_number)}')
 elif option == 3:
-    print('OPERATION OF MULTIPLICATION')
+    print('OPERATION OF MULTIPLICATION:')
     print(f'{first_number} * {second_number} = {mult_option(first_number, second_number)}')
 elif option == 4:
     print('OPERATION OF DIVISION')
     print(f'{first_number} / {second_number} = {div_option(first_number, second_number)}')
 elif option == 5:
-    print('OPERATION OF DIVISION WITH MODULE')
+    print('OPERATION OF DIVISION WITH MODULE:')
     print(f'{first_number} / {second_number} = {div_option(first_number, second_number)}')
     print(f'{first_number} % {second_number} = {module_option(first_number, second_number)}')
 elif option == 6:
-    print('OPERATION OF ENTIRE PART OF THE DIVISION')
+    print('OPERATION OF ENTIRE PART OF THE DIVISION:')
     print(f'{first_number} // {second_number} = {entire_part(first_number, second_number)}')
+elif option == 7:
+    print(8*'>', 'OPERATION OF SQUARE ROOT:')
+    print(8*'>', f'√{one_number} = {square_root(one_number)}')
 else:
     print(20 * '*')
     print('ERRO, REBOOT THE CODE')
